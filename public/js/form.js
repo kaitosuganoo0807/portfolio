92adf9293bea4646cf2bf440b70c5dca852f0230
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-01-27 19:06:36 +0900

    update

---
+++ public/js/form.js
@@ -0,0 +1,19 @@
+$(function() {
+  $('#form').submit(function(){
+      $.ajax({
+          url: $('#form').attr('action'),
+          data: $('#form').serialize(),
+          type: 'POST',
+          dataType: 'xml',
+          statusCode: {
+              0: function() {
+                  //成功した際の処理を書きます。
+              },
+              200: function() {
+                  //成功した際の処理を書きます。
+              }
+          }
+      });
+      return false;
+  });
+});
